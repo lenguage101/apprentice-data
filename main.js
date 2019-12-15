f1a05bfb62c840fecb68bea95efa6658b1f3d301
Tommy Leng <tommyleng@yahoo.com>
2019-12-14 20:43:07 -0800

    Add HTML elements via JS

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let wateringPlants;
 let days = 84;
 
+let schedule = document.getElementById('schedule');
 fetch('https://gist.githubusercontent.com/lenguage101/315dfdebc2c010ee899a2ddcaf79ed31/raw/71cde9402a8cf5753aa14f757eecfc24eb749bf3/apprentice-data.json')
     .then((resp) => {
         return resp.json();
@@ -23,36 +24,66 @@ fetch('https://gist.githubusercontent.com/lenguage101/315dfdebc2c010ee899a2ddcaf
         //Loops through the wateringPlant database
         for (let i = 0; i < wateringPlants.length; i++) {
             //Loops through the array of days of each plants to see 
-            let addExtraWeek = i + 1;
-            console.log(wateringPlants[i])
+            console.log(wateringPlants[i]);;
+            let list = document.createElement('ul');
+            let para = document.createElement('p');
+            let plantName = document.createTextNode(`${wateringPlants[i].name} is watered every ${wateringPlants[i].water_after}`);
+            para.appendChild(plantName);
+            schedule.appendChild(para);
+            schedule.appendChild(list)
             for (let j = 0; j < wateringPlants[i].days.length; j++) {
                 let daysArray = wateringPlants[i].days;                
                 let weekNumber = daysArray[j] / 7;
+                let li = document.createElement('li')
                 weekNumber = Math.floor(weekNumber);
                 if (daysArray[j] > days) {
-                    console.log('Skipped');
+                    // console.log('Skipped');
                 } else if (daysArray[j] % 7 === 1) {
-                    console.log(`${wateringPlants[i].name} was watered on Day ${daysArray[j]} of week ${weekNumber + 1} on Monday`);
+                    let liText = document.createTextNode( `Week ${weekNumber + 1} - ${wateringPlants[i].name} was watered on ` 
+                                                        + `Day ${daysArray[j]} on Monday`);
+                    li.appendChild(liText);
+                    list.appendChild(li);
                 } else if (daysArray[j] % 7 === 2) {
-                    console.log(`${wateringPlants[i].name} was watered of Day ${daysArray[j]} of week ${weekNumber + 1} on Tuesday`);
+                    let liText = document.createTextNode( `Week ${weekNumber + 1} - ${wateringPlants[i].name} was watered on ` 
+                                                        + `Day ${daysArray[j]} on Tuesday`);
+                    li.appendChild(liText);
+                    list.appendChild(li);
                 } else if (daysArray[j] % 7 === 3) {
-                    console.log(`${wateringPlants[i].name} was watered of Day ${daysArray[j]} of week ${weekNumber + 1} on Wednesday`);
-                } else if (daysArray[j] % 7 === 4) {
-                    console.log(`${wateringPlants[i].name} was watered of Day ${daysArray[j]} of week ${weekNumber + 1} on Thursday`);
+                    let liText = document.createTextNode( `Week ${weekNumber + 1} - ${wateringPlants[i].name} was watered on ` 
+                                                        + `Day ${daysArray[j]} on Wednesday`);
+                    li.appendChild(liText);
+                    list.appendChild(li);                } 
+                else if (daysArray[j] % 7 === 4) {
+                    let liText = document.createTextNode( `Week ${weekNumber + 1} - ${wateringPlants[i].name} was watered on ` 
+                                                        + `Day ${daysArray[j]} on Thursday`);
+                    li.appendChild(liText);
+                    list.appendChild(li);
                 } else if (daysArray[j] % 7 === 5) {
-                    console.log(`${wateringPlants[i].name} was watered of Day ${daysArray[j]} of week ${weekNumber + 1} on Friday`);
+                    let liText = document.createTextNode( `Week ${weekNumber + 1} - ${wateringPlants[i].name} was watered on ` 
+                                                        + `Day ${daysArray[j]} on Friday`);
+                    li.appendChild(liText);
+                    list.appendChild(li);                
                 } else if (daysArray[j] % 7 === 6) {
-                    if ((daysArray[j] - 1) % 7 !== 6 || (daysArray[j] - 1) % 7 !== 0) {
-                        console.log(`${wateringPlants[i].name} was watered on Day ${daysArray[j] - 1} of week ${weekNumber + 1} on Friday ` + 
-                                    `instead of Day ${daysArray[j]} on Saturday`)
+                    if ( (daysArray[j] - 1) % 7 !== 6 || 
+                         (daysArray[j] - 1) % 7 !== 0 ) {
+                            let liText = document.createTextNode( `Week ${weekNumber + 1} - ${wateringPlants[i].name} was watered on ` 
+                                                                + `Day ${daysArray[j] - 1} on Friday instead of Day ${daysArray[j]} `
+                                                                + `since falls on a Saturday`);
+                            li.appendChild(liText);
+                            list.appendChild(li);
                     }
                 } else {
-                    if ((daysArray[j] + 1) % 7 !== 6 || (daysArray[j] + 1) % 7 !== 0) {
+                    if ( (daysArray[j] + 1) % 7 !== 6 || 
+                         (daysArray[j] + 1) % 7 !== 0 ) {
                         if(daysArray[j] + 1 >= 84){
-                            console.log(`End of Cycle`)
+                            console.log(`End of Cycle`);
                         } else {
-                            console.log(`${wateringPlants[i].name} was watered on Day ${daysArray[j] + 1} of week ${weekNumber + 1} on Monday ` +
-                                        `instead of Day ${daysArray[j]} on Sunday`)                        }
+                            let liText = document.createTextNode( `Week ${weekNumber + 1} - ${wateringPlants[i].name} was watered on ` 
+                                                                + `Day ${daysArray[j] + 1} on Monday instead of Day ${daysArray[j]} `
+                                                                + `since falls on a Sunday`);
+                            li.appendChild(liText);
+                            list.appendChild(li);
+                        }
                     }
                 } 
             }           
